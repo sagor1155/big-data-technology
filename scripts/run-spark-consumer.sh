24a8dc8e32600587589616db48d7ae90e43c7ab4
sagor sarker <47176181+sagor1155@users.noreply.github.com>
2024-02-05 15:31:54 +0600

    Update run-spark-consumer.sh

--- scripts/run-spark-consumer.sh
+++ scripts/run-spark-consumer.sh
@@ -1,3 +1,3 @@
+#!/bin/bash
 echo "............Starting Spark Streaming Consumer Application..........."
 spark-submit --class "cs523.spark.streaming.SparkKafkaConsumer" --master local[*]  '/home/cloudera/workspace/spark-kafka-streaming/target/spark-consumer-jar-with-dependencies.jar'
-
